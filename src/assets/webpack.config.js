199472f451fe36f3b5dcf57590d24fe0af681bae
Kaluã Bentes <kaluanbentes@gmail.com>
2019-11-19 21:37:52 -0300

    Complement webpack config

--- src/assets/webpack.config.js
+++ src/assets/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const BundleTracker = require("webpack-bundle-tracker");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = () => process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: "./js/index.js",
@@ -9,16 +12,26 @@ module.exports = {
     path: path.resolve(__dirname, "public/dist")
   },
   mode: process.env.NODE_ENV,
+  devtool: isDev() && "inline-source-map",
   module: {
     rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"]
+        use: [
+          isDev() ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
+        ]
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new BundleTracker({ filename: "./webpack-stats.json" })
+    new BundleTracker({ filename: "./webpack-stats.json" }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
   ]
 };
